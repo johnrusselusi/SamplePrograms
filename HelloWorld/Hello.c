dffb07fc0e7578b578e4f3db85dc14a683ac0f98
John Russel Usi <j.usi@klab.com>
2014-09-05 12:22:45 +0800

    Reverted

--- HelloWorld/Hello.c
+++ HelloWorld/Hello.c
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-#include<iostream>
-using namespace std;
-
-int main()
-{
-	string name;
-	scanf(name);
-	printf("Hello World!/s",name);
-=======
 #include<stdio.h>
 
 int main()
 {
 	printf("Hello World!");
->>>>>>> 9e169ef96abfb27ba213ca5167eccb4a11f24c45
 	return 0;
 }
